674ddde94d71cbce1824054f0cd49996f3c9cfbe
Eduardo Flores <eduflornet@gmail.com>
2016-04-25 17:10:46 +0200

    SharePoint Add-inn con Custom Action Item Menu enlazado a realizar búsquedas en Document Library
    SharePoint Add-inn con Custom Action Item Menu

---
+++ SPCustomActionSearch/SPCustomActionSearch/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="a98c05e1-a21b-48eb-a232-cd15a26ee87e" description="SharePoint Add-in Feature" featureId="a98c05e1-a21b-48eb-a232-cd15a26ee87e" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SPCustomActionSearch Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="9b40b8d1-326a-4b80-8c23-9e05be53e25a" />
+    <projectItemReference itemId="01cfd620-8e0b-49f8-880e-b1c5715294c7" />
+    <projectItemReference itemId="41f83718-d3fc-4ec4-8816-f03d7a0b4d8c" />
+    <projectItemReference itemId="48c53f14-86d5-4584-b2b5-a8cadc37c913" />
+  </projectItems>
+</feature>
---
+++ SPCustomActionSearch/SPCustomActionSearch/Scripts/App.js
@@ -0,0 +1,123 @@
+﻿'use strict';
+
+/*
+2.	Crear las siguientes variables de acceso publico
+a.	Context
+b.	Web
+c.	User
+d.	Hostweburl
+e.	Appweburl
+f.	Title
+* 
+ */ 
+var context;
+var web;
+var user;
+var currentItem;
+var hostweburl;
+var appweburl;
+var title;
+
+/**
+ * 3.	Crear una función que busque en la lista por el nombre del documento 
+ * y que cargue la información de la búsqueda en una capa
+ */
+
+function DoSearch() {
+    var query = $('#txtSearch').val();
+    getSearchResults(query);
+}
+
+function getSearchResults(queryText) {
+    $('#search-title').text('Search results for [' + queryText + ']');
+
+    var searchUrl = appweburl + "/_api/search/query?querytext='" + queryText + "'&trimduplicates=false";
+    var executor = new SP.RequestExecutor(appweburl);
+    executor.executeAsync(
+    {
+        url: searchUrl,
+        method: "GET",
+        headers: { "Accept": "application/json; odata=verbose" },
+        success: onGetSearchResultsSuccess,
+        error: onGetSearchResultsFail
+    });
+}
+
+function onGetSearchResultsSuccess(data) {
+    var jsonObject = JSON.parse(data.body);
+    var results = jsonObject.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.results;
+    if (results.length == 0) {
+        $('#search-results').text('No items were found');
+    }
+    else {
+        var searchResultsHtml = '';
+        $.each(results, function (index, result) {
+            searchResultsHtml += "<a target='_blank' href='" + result.Cells.results[6].Value + "'>" + result.Cells.results[6].Value + "</a><br/>";
+        });
+
+        $("#search-results").html(searchResultsHtml);
+    }
+}
+
+function onGetSearchResultsFail(data, errorCode, errorMessage) {
+    $('#search-results').text('An error occurred during search - ' + errorMessage);
+}
+
+/**
+ * 4.	Crear las funciones necesarias para obtener los siguientes datos del query string
+a.	ListId
+b.	SPHostUrl
+c.	SPAppWebUrl
+d.	ItemId
+
+ */
+
+function getQueryStringParameter(paramToRetrieve) {
+    var params =
+        document.URL.split("?")[1].split("&");
+    var strParams = "";
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split("=");
+        if (singleParam[0] == paramToRetrieve)
+            return singleParam[1];
+    }
+}
+
+function GetListId() {
+    return decodeURIComponent(getQueryStringParameter("ListID"));
+}
+
+function GetHostSiteUrl() {
+    return decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+}
+
+function GetAppSiteUrl() {
+    return decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+}
+
+
+/**
+ * 5.	En el document ready crear el código necesario para enlazar las variables publicas
+ * 
+ */
+
+$(document).ready(function () {
+    appweburl = GetAppSiteUrl();
+    hostweburl = GetHostSiteUrl();
+
+    var scriptbase = hostweburl + "/_layouts/15/";
+    $.getScript(scriptbase + "SP.RequestExecutor.js");
+
+    var clientContext = new SP.ClientContext.get_current();
+    var parentCtx = new SP.AppContextSite(clientContext, hostweburl);
+    var web = parentCtx.get_web();
+    clientContext.load(web);
+    var listId = GetListId();
+    list = web.get_lists().getById(listId);
+    clientContext.load(list);
+    var itemId = GetItemId();
+    currentItem = list.getItemById(itemId);
+    clientContext.load(currentItem);
+
+    clientContext.executeQueryAsync(onListLoadSucceeded, onRequestFailed);
+});
